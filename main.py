c164e3bd678421aa8332b386d679bbd3e7ac359f
Bhagya RanbandiDewage <sa21799@student.samk.fi>
2024-11-16 20:24:12 +0200

    nice

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("hello collaborator")
 
-<<<<<<< HEAD
-print("what is the topic you want to discuss?")
-=======
 print("Nice. Let's work together")
->>>>>>> 4772ce96a6dfd70b531e54130af74365b72508c0
+
+print("hello collaborator")
+
+print("what is the topic you want to discuss?")
